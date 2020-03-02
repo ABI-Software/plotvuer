9ace3dec2aaf2231ef477a97cd025ab293e43936
Jesse Khorasanee <jessekhorasanee@gmail.com>
2020-03-02 17:07:56 +1300

    Maintainability update:
     - Switch to using ES6 class definition
     - Remove unused functions
     - Switch from using a 'this' alias to arrow functions

--- src/components/csv_manager.js
+++ src/components/csv_manager.js
@@ -1,33 +1,36 @@
 var Papa = require('papaparse')
 
-function Sparccsv() {
+class Sparccsv {
 
-  var _this = this
-  _this.csv_data = undefined
+  constructor(){
 
-  this.loadFile = function (file_url) {
-    return new Promise(function(resolve){
+  }
+
+
+
+  loadFile(file_url) {
+    return new Promise( (resolve) => {
       fetch(file_url).then(response => response.text())
         .then( data => {
-          _this.csv_data = Papa.parse(data)
+          this.csv_data = Papa.parse(data)
           resolve() 
         })
       })
   }
 
-  this.transposeSelf = function () {
-    var tempData = transpose(_this.csv_data.data)
-    _this.csv_data.data = tempData
+  transposeSelf() {
+    var tempData = this.transpose(this.csv_data.data)
+    this.csv_data.data = tempData
   }
 
-  this.getDataType = function () {
-    var first = _this.getHeaders()[0]
+  getDataType() {
+    var first = this.getHeaders()[0]
     if (first.toLowerCase().includes('time')) {
       return 'scatter'
     } else if (first.toLowerCase().includes('name')) {
       return 'bar'
     }    
-    var first_col = _this.getColoumnByIndex(0)
+    var first_col = this.getColoumnByIndex(0)
     if (Number(first_col[4]) > Number(first_col[3])){
       return 'scatter'
     } else {
@@ -35,43 +38,43 @@ function Sparccsv() {
     }
   }
 
-  this.getXaxis = function(){
-    var firstHeader = _this.csv_data.data[0][0]
+  getXaxis(){
+    var firstHeader = this.csv_data.data[0][0]
     return firstHeader.split('-').join(' ').split('_').join(' ')
   }
 
-  this.getTitle = function(url){
+  getTitle(url){
     var title = url.split('/').pop() //grab file from url
     title = title.split('-').join(' ').split('_').join(' ').split('.')[0] //Process file name into title
     return title
   }
 
-  this.getHeaders = function () {
-    return _this.csv_data.data[0]
+  getHeaders() {
+    return this.csv_data.data[0]
   }
 
-  this.getAllData = function () {
-    return _this.csv_data.data
+  getAllData() {
+    return this.csv_data.data
   }
 
-  this.getSampleRate = function () {
-    return 1 / (_this.csv_data.data[1][1] - _this.csv_data.data[1][0])
+  getSampleRate() {
+    return 1 / (this.csv_data.data[1][1] - this.csv_data.data[1][0])
   }
 
-  this.getColoumnByIndex = function (index) {
-     let col = _this.csv_data.data.map((row) => { return row[index] })
+  getColoumnByIndex(index) {
+     let col = this.csv_data.data.map((row) => { return row[index] })
      col.shift()
      return col
   }
 
-  this.getHeaderByIndex = function (index) {
-    return _this.csv_data.data[0][index]
+  getHeaderByIndex(index) {
+    return this.csv_data.data[0][index]
   }
 
-  this.getColoumnByName = function (column_name) {
+  getColoumnByName(column_name) {
     var column_index = 0
-    for (let i in _this.csv_data.data[0]) {
-      if (_this.csv_data.data[0][i] === column_name) {
+    for (let i in this.csv_data.data[0]) {
+      if (this.csv_data.data[0][i] === column_name) {
         column_index = i
       }
     }
@@ -81,90 +84,28 @@ function Sparccsv() {
     return this.getColoumnByIndex(column_index)
   }
 
-  this.export = function(selectedChannels){
+  export(selectedChannels){
     var headerList = []
     var selectedData = []
-    headerList.push(_this.getHeaderByIndex(0))
-    selectedData.push(_this.getColoumnByIndex(0))
+    headerList.push(this.getHeaderByIndex(0))
+    selectedData.push(this.getColoumnByIndex(0))
     for (let i in selectedChannels){
       headerList.push(selectedChannels[i])
-      selectedData.push(_this.getColoumnByName(selectedChannels[i]))
+      selectedData.push(this.getColoumnByName(selectedChannels[i]))
     }
     var filename = 'sparc-export-' + Math.random().toString(36).slice(-5)  + '.csv'
-    downloadCSV({
+    this.downloadCSV({
       filename: filename,
-      data: transpose(selectedData),
+      data: this.transpose(selectedData),
       columns: headerList
     })
   }
 
-  this.exportForOpenCOR = function(state){
-    var headerList = []
-    var data 
-    var selectedData = []
-    headerList.push(_this.getHeaderByIndex(0))
-    selectedData.push(_this.getColoumnByIndex(0))
-    for (let i in state.selectedChannels){
-      headerList.push(state.selectedChannels[i])
-      selectedData.push(_this.getColoumnByName(state.selectedChannels[i]))
-    }
-    data = transpose(selectedData)
-    data[0] = convertHeadersToOpencor(data[0])
-    var filename = 'sparc-export-' + Math.random().toString(36).slice(-5)  + '.csv'
-    downloadCSV({
-      filename: filename,
-      data: data,
-      columns: headerList
-    })
-  }
-
-  var transpose = function(array){
+  transpose(array){
     return array[0].map((col, i) => array.map(row => row[i]));
   }
 
-  var convertHeadersToOpencor = function(csvHeaders){
-    var headers = [...csvHeaders]
-    if (headers[0].toLowerCase().includes('seconds')) {
-      headers[0] = 'environment | time (second)'
-    } else if (!headers[0].toLowerCase().includes('minute')) {
-      headers[0] = 'environment | time (minute)'
-    } else if (!headers[0].toLowerCase().includes('ms')) {
-      headers[0] = 'environment | time (millisecond)'
-    } else if (!headers[0].toLowerCase().includes('millisecond')) {
-      headers[0] = 'environment | time (millisecond)'
-    } else {
-      headers[0] = 'environment | time (unknown unit)'
-    }
-
-    for (let i in headers){
-      if (i === 0){
-        continue
-      } 
-      if (headers[i].toLowerCase().includes('mv') || headers[i].toLowerCase().includes('millivolts') ){
-        headers[i].replace('mv','')
-        headers[i].replace('millivolts','')
-        headers[i].replace('(','')
-        headers[i].replace(')','')
-        headers[i] += ' | V (millivolt)'
-      }
-      else if (headers[i].toLowerCase().includes(' v') || headers[i].toLowerCase().includes('volts')){
-        headers[i].replace('mv','')
-        headers[i].replace('volts','')
-        headers[i].replace('(','')
-        headers[i].replace(')','')
-        headers[i] += ' | V (volt)'
-      }
-      else if (headers[i].toLowerCase().includes('(')){
-        var unit = '(' + headers[i].split('(')[1].split(')')[0] + ')' 
-        headers[i] = headers[i].split('(')[0] + ' | ' + unit[0].toUpperCase() + ' ' + unit 
-      } else {
-        headers[i] += ' | x (Unknown Unit)'
-      }
-    }
-    return headers
-  }
-
-  var downloadCSV = (args) => {  
+  downloadCSV = (args) => {  
 
     /* Use like so: 
      downloadCSV({ 
