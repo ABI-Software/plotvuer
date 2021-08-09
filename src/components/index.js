baa537d69f3048242d4ea4aa28424a5e9ecda23f
hsorby <h.sorby@auckland.ac.nz>
2021-08-09 14:16:39 +1200

    Package all plot types into the same bundle.

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import PlotVuer from "./PlotVuer.vue"
+import PlotVuer from './PlotVuer.vue'
 
-export {
-  PlotVuer
-};
+export {PlotVuer}
+
+import HeatmapPlot from './HeatmapPlot'
+import TimeseriesPlot from './TimeseriesPlot'
+if (HeatmapPlot || TimeseriesPlot) {
+  // Are these things hidden?
+}
